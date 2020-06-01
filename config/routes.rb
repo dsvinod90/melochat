43f6c95e639724a359a16e2cb9a68103ce916743
VinodDalavai <dsvinod.1990@icloud.com>
2020-06-02 03:16:08 +0530

    Add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
   get 'welcome/home', to: 'welcome#home'
   root 'welcome#home'
   resources :blogs
