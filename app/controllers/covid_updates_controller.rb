046235bc0d62470ed5a1bdf5a4c8b95905c63c39
VinodDalavai <dsvinod.1990@icloud.com>
2020-06-06 21:34:00 +0530

    Add more charts

--- app/controllers/covid_updates_controller.rb
+++ app/controllers/covid_updates_controller.rb
@@ -7,5 +7,10 @@ class CovidUpdatesController < ApplicationController
         @country_wise_summary = global_summary['Countries']
             .map{ |hash| { hash['CountryCode'] => hash[status]} }
             .reduce(:merge)
+        country = params[:country] || 'india'
+        @all_status = Rails.cache.fetch("all_status_#{country}", expires_in: 2.hours) do
+            ::Services::GetCovidUpdates.new.get_stats(country)
+        end
+        @latest_status = @all_status.last
     end
 end
