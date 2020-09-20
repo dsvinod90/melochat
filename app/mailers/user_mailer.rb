37c5673c6df87264d746fa28b43c1009ce1a7937
VinodDalavai <dsvinod.1990@icloud.com>
2020-09-20 21:19:33 +0530

    Fix email template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ApplicationMailer
   def newsletter
     @user = params[:user]
+    unsubscribe_url = unsubscribe_url(@user.token)
+    headers['List-Unsubscribe'] = "<#{unsubscribe_url}>"
     mail(to: @user.email, subject: 'Hypocryte - This week\'s newsletter')
   end
 end
