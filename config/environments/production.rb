d2e55db38984b55ebdc5d210062bba812c89db2f
VinodDalavai <dsvinod.1990@icloud.com>
2020-09-24 02:48:12 +0530

    Integrate Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,16 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'www.hypocryte.monster', protocol: 'https' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'mail.privateemail.com',
-    port:                 587,
-    domain:               'hypocryte.monster',
-    user_name:            'mail@hypocryte.monster',
-    password:             Rails.application.credentials[:email][:pass_key],
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
