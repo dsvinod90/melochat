9cd14be5592d23394e276449ed92d15e71a53873
VinodDalavai <dsvinod.1990@icloud.com>
2020-10-11 10:24:57 +0530

    Refactor Templates

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,22 +3,44 @@
 class AdminsController < ApplicationController
   before_action :authenticate_admin!
 
-  def index
-    @users  = User.all
+  def show_admins_list
     @admins = Admin.all
+  end
+
+  def show_users_list
+    @users = User.all
+  end
+
+  def email_templates
+    @templates = SendgridTemplate.all
+  end
+
+  def newsletter_settings
+    @users = User.where(subscribed: true)
   end
 
   def send_newsletters
     user_ids = params[:user_id]
     if user_ids.present? && user_ids != 'all'
       SendNewsletterWorker.perform_async(user_ids)
-      redirect_to(admins_path, flash: { success: I18n.t('admin.newsletter_sent') })
+      redirect_to(newsletter_settings_admins_path, flash: { success: I18n.t('admin.newsletter_sent') })
     elsif user_ids == 'all'
       all_user_ids = User.all.pluck(:id)
       SendNewsletterWorker.perform_async(all_user_ids)
-      redirect_to(admins_path, flash: { success: I18n.t('admin.newsletter_sent') })
+      redirect_to(newsletter_settings_admins_path, flash: { success: I18n.t('admin.sent_to_all') })
     else
-      redirect_to(admins_path, flash: { alert: I18n.t('admin.users_not_selected') })
+      redirect_to(newsletter_settings_admins_path, flash: { alert: I18n.t('admin.users_not_selected') })
+    end
+  end
+
+  def update_templates
+    template_id = params[:template_id]
+    activation_status = params[:active]
+    template = SendgridTemplate.find_by(id: template_id)
+    if template.update_attributes(active: activation_status)
+      redirect_to(email_templates_admins_path, flash: { success: I18n.t('admin.template_updated') })
+    else
+      redirect_to(email_templates_admins_path, flash: { alert: template.errors.full_messages })
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_190518) do
+ActiveRecord::Schema.define(version: 2020_10_11_045248) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2020_10_06_190518) do
     t.string "version"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "active"
   end
 
   create_table "users", force: :cascade do |t|
